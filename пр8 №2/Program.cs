c982187c6b090e8c8bd175fb008c4e379c730855
Виталий <@DESKTOP-NTOVGEQ>
2022-10-22 01:45:33 +0300

    Добавьте файлы проекта.

---
+++ пр8 №2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число : ");
+            int n = int.Parse(Console.ReadLine());
+            int a = n / 100;
+            int c = n % 10;
+            int b = n % 100 / 10;
+            if (a == b && b == c)
+            {
+                Console.WriteLine("Цифры трехзначного числа одинаковые");
+            }
+            else
+            {
+                Console.WriteLine("Цифры трехзначного числа не одинаковые");
+            }
+        }
+    }
+}
